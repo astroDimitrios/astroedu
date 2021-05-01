a889c1ffff884f6b46b3c04a932c7084fcf962c4
Dimitrios Theodorakis <dtheodorakisphoto@gmail.com>
2021-05-01 15:56:06 +0100

    Source update
    Include data and interactives in /src/

--- src/astroedu/__main__.py
+++ src/astroedu/__main__.py
@@ -1,21 +1,24 @@
-from configparser import ConfigParser
-from os import path
 import subprocess
+from importlib_resources import files, is_resource
+from importlib.util import find_spec
+from configparser import ConfigParser
+import fire
 
-def load_astroedu_path():
-    from site import getusersitepackages
-    return getusersitepackages() + '/astroedu/'
+from astroedu.__build__ import get_astroedu_path
+
+def main():
+    fire.Fire(startup)
 
 def load_astroedu_config():
-    if not path.exists(config_file_name):
+    if not is_resource('astroedu', 'config.ini'):
         raise FileExistsError('No config.ini file found. Did you forget to run: \n astroedu build')
     config = ConfigParser()
-    config.read(config_file_name)
+    config.read(config_file_path)
     return config['astroedu-config']
 
-commands = ['interactive']
-astroedu_path = load_astroedu_path()
-config_file_name = 'config.ini'
+commands = ['build', 'interactive']
+astroedu_path = get_astroedu_path()
+config_file_name = '/config.ini'
 config_file_path = astroedu_path + config_file_name
 
 def startup(*args):
@@ -32,15 +35,14 @@ def startup(*args):
         build_path_config()
     elif command == 'interactive':
         interactive_name = args[1]
-        interactive_path = astroedu_path+'interactives/'+interactive_name+'.ipynb'
-        interactive_path = './interactives/'+interactive_name+'.ipynb'
-        if not path.exists(interactive_path):
-            raise FileExistsError(f'No interactive named {interactive_name}.')
-        call = ['jupyter', 'lab']
-        call_args = list(args[2:]) if len(args) > 2 else []
-        run_interactive = call + [interactive_path] + call_args
-        print(f'Loading interactive {interactive_name}!')
-        subprocess.run(run_interactive)
+        if not is_resource('astroedu.interactives', interactive_name+'.ipynb'):
+            raise FileExistsError(f'No interactive named {interactive_name} found.')
+        with files('astroedu').joinpath('interactives', interactive_name+'.ipynb') as p:
+            call = ['jupyter', 'lab']
+            call_args = list(args[2:]) if len(args) > 2 else []
+            run_interactive = call + [str(p)] + call_args
+            print(f'Loading interactive {interactive_name}!')
+            subprocess.run(run_interactive)
 
-# if __name__ == '__main__':
-#     startup('interactive', 'wiens_law', '--port', '9999')
+if __name__ == '__main__':
+    fire.Fire(startup)
